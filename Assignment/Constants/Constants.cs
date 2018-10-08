05ae39b5e27846e21ee64b80cd3e1abac99a7d3c
Ankit Bali <ankit.bali@nagarro.com>
2018-10-08 11:46:45 +0530

    Added constants

--- Assignment/Constants/Constants.cs
+++ Assignment/Constants/Constants.cs
@@ -2,8 +2,8 @@ namespace Assginment.Constants
 {
     public static class ConstValues
     {
-        public const string FilePathProducts = @"D:\My\Projects\VsCode\Assignment\Assignment\App_Data\Products.json";
+        public const string FilePathProducts = @".\App_Data\Products.json";
 
-        public const string FilePathManufacturers = @"D:\My\Projects\VsCode\Assignment\Assignment\App_Data\Maufacturer.json";
+        public const string FilePathManufacturers = @".\App_Data\Maufacturer.json";
     }
 }
